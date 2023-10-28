97ce1feb8deb03f68bcab765d7b9acdbb05206f3
vanamuthuV <109649687+vanamuthuV@users.noreply.github.com>
2023-10-28 09:39:29 +0530

    First Sub Commit

--- src/Exercise - 03/Conditional Rendering 2/Riddle.jsx
+++ src/Exercise - 03/Conditional Rendering 2/Riddle.jsx
@@ -9,23 +9,11 @@ export const Render = () => {
     const [data, setData] = useState(RiddleData)
 
     const Reveal = (id) => {
-        console.log(id)
-        setData(prev => {
-            const newArray = []
-            for (let i = 0; i < prev.length; i++) {
-                if (prev[i].id === id) {
-                    const newValue = {
-                        ...prev[i],
-                        isShown : !prev[i].isShown
-                    }
-                    console.log(newValue)
-                    newArray.push(newValue)
-                } else {
-                    newArray.push(prev[i])
-                }
-            }
-            return newArray
-        })
+      setData((prev) => {
+        return prev.map((item) => {
+          return item.id === id ? { ...item, isShown: !item.isShown } : item;
+        });
+      });
     }
 
     return (
--- src/Exercise - 03/Conditional Rendering 2/RiddleBody.jsx
+++ src/Exercise - 03/Conditional Rendering 2/RiddleBody.jsx
@@ -2,9 +2,14 @@ import React from "react";
 
 export const Riddle = (props) => {
     return (
-        <div>
-            <p className="P1" onClick={() => props.handleclick(props.id)}>Riddle : {props.Question }</p>
-            {props.isShown ? <p className="P2" >Answer : {props.Answer}</p> : "" }
+      <div>
+        <div className="Answer-Div">
+          <p className="P1">Riddle : {props.Question}</p>
+          <button onClick={() => props.handleclick(props.id)}>
+            Reveal Answer
+          </button>
         </div>
-    )
+        {props.isShown && <p className="P2">Answer : {props.Answer}</p>}
+      </div>
+    );
 }
